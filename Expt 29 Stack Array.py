55cc3d80852e21873bc69cf74be4943abc4f1dba
Harish Ramaswamy <72350565+RHarish1@users.noreply.github.com>
2023-11-01 19:46:23 +0530

    Update Expt 29 Stack Array.py

--- Expt 29 Stack Array.py
+++ Expt 29 Stack Array.py
@@ -23,33 +23,3 @@ stack.push(3)
 print("Stack:", stack.items)
 print("Pop:", stack.pop())
 print("Size:", stack.size())
-
-#Using linked list
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class Stack:
-    def __init__(self):
-        self.top = None
-
-    def push(self, item):
-        new_node = Node(item)
-        new_node.next = self.top
-        self.top = new_node
-
-    def pop(self):
-        if not self.is_empty():
-            item = self.top.data
-            self.top = self.top.next
-            return item
-
-    def is_empty(self):
-        return self.top is None
-
-stack = Stack()
-stack.push(1)
-stack.push(2)
-stack.push(3)
-print("Pop:", stack.pop())
